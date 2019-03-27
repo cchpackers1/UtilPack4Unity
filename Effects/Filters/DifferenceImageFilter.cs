b811b857586d5e91471e67b32fe59d57b80de861
shimizuyudai <shimizu.yudai.0730@gmail.com>
2019-03-28 06:37:45 +0900

    update

--- Effects/Filters/DifferenceImageFilter.cs
+++ Effects/Filters/DifferenceImageFilter.cs
@@ -2,122 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DifferenceImageFilter : GrabbableImageFilter
-{
+public class DifferenceImageFilter : GrabbableImageFilter {
     [SerializeField]
-    bool isSync;
-    [SerializeField]
-    TextureHolderBase syncTextureHolder;
-    [SerializeField]
-    Camera2RenderTexure cam2renderTex;
+    protected float threshold;
 
-    [SerializeField]
-    float threshold;
-    RenderTexture cache, preFrame;
-    bool isRefresh;
-    Material neutralMaterial;
-    bool isInit;
-    [SerializeField]
-    float interval;
-
-    protected override void Awake()
+    protected RenderTexture[] rts;
+    public void Capture()
     {
-        var shader = Shader.Find("Hidden/NeutralImageFilter");
-        neutralMaterial = new Material(shader);
-        cam2renderTex.InitializedEvent += Cam2renderTex_InitializedEvent;
-        base.Awake();
-        if (isSync)
-        {
-            syncTextureHolder.RefreshTextureEvent += TextureHolder_RefreshTextureEvent;
-        }else
-        {
-            StartCoroutine(UpdateRoutine());
-        }
-        
+        Graphics.Blit(rts[0], rts[1]);
     }
-
-    private void Cam2renderTex_InitializedEvent(RenderTexture renderTexture)
-    {
-        Release();
-        cache = new RenderTexture(renderTexture.width, renderTexture.height, renderTexture.depth);
-        preFrame = new RenderTexture(renderTexture.width, renderTexture.height, renderTexture.depth);
-        isInit = false;
-    }
-
-    IEnumerator UpdateRoutine()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(interval);
-            isRefresh = true;
-        }
-    }
-
-    bool isSave;
 
     private void Update()
     {
-        if (Input.GetKeyDown("s"))
-        {
-            isSave = true;
-        }
-    }
-
-    private void TextureHolder_RefreshTextureEvent()
-    {
-        isRefresh = true;
+        this.material.SetFloat("_Threshold", threshold);
+        this.material.SetTexture("_CacheTex",rts[1]);
     }
 
     public override void Filter(Texture source, RenderTexture destination)
     {
-        material.SetFloat("_Threshold", threshold);
-
-        if (isRefresh)
-        {
-            //前フレームの保存
-            material.EnableKeyword("IsRefresh");
-            material.SetTexture("_PreFrameTex", preFrame);
-            cacheFrame("_CacheTex", source, cache, material);
-            material.DisableKeyword("IsRefresh");
-            isRefresh = false;
-            cacheFrame("_PreFrameTex", source, preFrame, neutralMaterial);
-            isRefresh = false;
-        }
-        base.Filter(source, destination);
+        SecureRenderTexures(source);
+        Graphics.Blit(source, rts[0]);
+        base.Filter(rts[0], destination);
     }
 
-    void cacheFrame(string param, Texture source, RenderTexture dst, Material material)
+    private void SecureRenderTexures(Texture texture)
     {
-        Graphics.Blit(source, dst, material);
-        material.SetTexture(param, dst);
+        if (rts == null)
+        {
+            InitRenderTextures(texture);
+        }
+        else if (texture.width != this.rts[0].width || texture.height != this.rts[0].height)
+        {
+            InitRenderTextures(texture);
+        }
     }
+
+    private void InitRenderTextures(Texture texture)
+    {
+        Release();
+        this.rts = new RenderTexture[] { new RenderTexture(texture.width, texture.height, 24),
+                 new RenderTexture(texture.width, texture.height, 24) };
+    }
+
+
 
     void Release()
     {
-        if (this.preFrame != null)
+        if (this.rts != null)
         {
-            preFrame.Release();
-            DestroyImmediate(preFrame);
-            preFrame = null;
-        }
-
-        if (this.cache != null)
-        {
-            cache.Release();
-            DestroyImmediate(cache);
-            cache = null;
+            for (var i = 0; i < rts.Length; i++)
+            {
+                rts[i].Release();
+                DestroyImmediate(rts[i]);
+                rts[i] = null;
+            }
+            this.rts = null;
         }
     }
 
     protected override void OnDestroy()
     {
-        base.OnDestroy();
         Release();
-        if (neutralMaterial != null)
-        {
-            Destroy(this.neutralMaterial);
-            this.neutralMaterial = null;
-        }
+        base.OnDestroy();
     }
-
 }
